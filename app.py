6833cd767fe44623224cf1aaec1e6efeec3e7868
Aymeric Weinbach <397730+zecloud@users.noreply.github.com>
2023-08-14 19:12:49 +0200

    cleaning and send filename instead of path

--- app.py
+++ app.py
@@ -19,30 +19,24 @@ hftoken = os.environ.get('YOUR_HF_TOKEN')
 #print('>>>>>>>>DAPR_HTTP_PORT : '+ daprPort )
 #print('>>>>>>>>DAPR_GRPC_PORT : '+ daprGRPCPort )
 app = App()
-#@app.route("/queueinput", methods=['POST'])
 @app.binding('queueinput')
 def incoming(request: BindingRequest):
-    #incomingtext = request.get_data().decode()
     incomingtext = base64.b64decode(request.text()).decode('utf-8')
     print(">>>>>>>Message Received: "+ incomingtext,flush=True)
     done = False
     with tempfile.NamedTemporaryFile(prefix="/outputs/tmp/",suffix=".wav",delete=True) as temp_file:
-        #newaudio_file = "/outputs/tempresult.wav"
         try:
             result = ffmpeg.input(incomingtext).output(temp_file.name).run(capture_stdout=True, capture_stderr=True,overwrite_output=True)
         except ffmpeg.Error as e:
             raise RuntimeError(f"Failed to load audio: {e.stderr.decode()}") from e
-        outputfile = "/outputs/Msg_"+datetime.datetime.now().strftime("%Y%m%d-%H%M%S-%f")+".txt"
-        done = process_message(temp_file.name,outputfile)
+        outputfile = "Msg_"+datetime.datetime.now().strftime("%Y%m%d-%H%M%S-%f")+".txt"
+        outputpath = "/outputs/"+outputfile
+        done = process_message(temp_file.name,outputpath)
     if done:
         print('>>>>>> Transcribe done.',flush=True)
         with DaprClient() as d:
              
-        
-        #url = 'http://localhost:'+daprPort+'/v1.0/bindings/bloboutput'
-        #contents = '{ "operation": "create", "data": {"message": "'+ outputfile+ '"} }'
-        #print(uploadcontents)
-        #requests.post(url, data = contents)
+    
             req_data = {'message':  outputfile} 
 
             resp = d.invoke_binding('queueoutput', 'create', json.dumps(req_data))
@@ -55,15 +49,13 @@ def incoming(request: BindingRequest):
             print('>>>>>> Transcribe done.',flush=True)
             d.close()
             app.stop()
-            #d.shutdown()
     else: 
         return "Error processing message!"
     return "Incoming message successfully processed!"
 
 def process_message(audio_file,text_file):
-#import gc 
+
     device = "cuda" 
-    #audio_file = "audio.mp3"
     batch_size = 16 # reduce if low on GPU mem
     compute_type = "float16" # change to "int8" if low on GPU mem (may reduce accuracy)
     # 1. Transcribe with original whisper (batched)
@@ -106,4 +98,3 @@ if __name__ == '__main__':
     app.run(50051)
     with DaprClient() as d:
         d.shutdown()
-    #app.run(host="localhost", port=6000, debug=False)
